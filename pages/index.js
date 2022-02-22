f44f1b5c3e00ac8be5e6b94923b26ecc8880aeb7
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2022-02-22 14:43:34 -0500

    merge index.js from amber_styling

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,56 @@
 import styled from 'styled-components';
-import Image from 'next/image';
-import BackgroundImage from '../public/pics/5thAveB.jpg';
+import Link from 'next/link';
 import { useIsTabletOrMobile } from '../hooks';
 import Footer from '../components/Footer';
 
-const X = 4;
-function Garden() {
-  return (
-    <Image
-      src={BackgroundImage} //pass in image
-      alt="A white house with a white fence and a new lovely new garden landscaping in the front, with a path down the middle"
-      width={6032 / X}
-      height={3024 / X}
-    />
-  );
-}
-
-const Y = 4;
-function GardenMobile() {
-  return (
-    <Image
-      src={BackgroundImage} 
-      alt="A white house with a white fence and a new lovely new garden landscaping in the front, with a path down the middle"
-      width={4032 / Y}
-      height={3024 / Y}
-    />
-  );
-}
-
 const Container = styled.div`
-  position: absolute;
-  top: 6rem;
-  width: 100%;
+  padding: 0;
+  margin: 0;
+  font-size: 2.5rem;
   display: flex;
-  flex-direction: column;
   align-items: center;
+  justify-content: center;
+  background: linear-gradient(to right, #122932, 50%, #e3c0d3 50%);
+  height: 100vh;
+`;
+
+const Heading = `Sean Keegan`;
+
+const Title = styled.h1`
+  position: fixed;
+  padding: 1.4rem;
+  top: 0;
+  margin: 5rem;
+  font-size: 3rem;
+  background-color: white;
+  /* This is the index font */
+  font-family: 'Orbitron', sans-serif;
+`;
+
+const StyledMusic = styled.h2`
+  margin-top: 30rem;
+  padding-right: 20rem;
+  text-align: center;
+  font-family: 'Bangers', cursive;
+`;
+
+const StyledTutor = styled.h2`
+  margin-top: 30rem;
+  padding-left: 20rem;
+  text-align: center;
+  font-family: 'Happy Monkey', cursive;
+`;
+
+const TutorQuote = `"Best Tutor EVER!!"`;
+const MusicQuote = `"Incredible, a must see!"`;
+
+const MusicParagraph = styled.p`
+  margin-top: 9rem;
+  color: white;
+`;
+const TutorParagraph = styled.p`
+  margin-top: 9rem;
+  color: white;
 `;
 
 const StyledFooterContainer = styled.div`
@@ -63,15 +79,22 @@ function HomePage({ copy }) {
   const isTabletOrMobile = useIsTabletOrMobile();
   return (
     <Container>
-      {!isTabletOrMobile && <Garden />}
-      {isTabletOrMobile && (
+      <Title>{Heading}</Title>
+      {!isTabletOrMobile && (
         <>
-          <div>
-            <GardenMobile />
-          </div>
-          <StyledFooterContainer>
-            <Footer copy={copy} />
-          </StyledFooterContainer>
+          <StyledMusic>
+            <Link href="/music/">Music</Link>
+            <MusicParagraph>{MusicQuote}</MusicParagraph>
+          </StyledMusic>
+          <StyledTutor>
+            <Link href="/tutoring/">Tutoring</Link>
+            <TutorParagraph>{TutorQuote}</TutorParagraph>
+          </StyledTutor>
+          {isTabletOrMobile && (
+            <StyledFooterContainer>
+              <Footer copy={copy} />
+            </StyledFooterContainer>
+          )}
         </>
       )}
     </Container>
