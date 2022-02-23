4758d3a639be144ad777cf764ab88373a9356f1c
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2022-02-23 17:12:44 -0500

    index merge, prob going to scrap though

--- pages/tutoring/index.js
+++ pages/tutoring/index.js
@@ -1,56 +1,30 @@
 import styled from 'styled-components';
-import Link from 'next/link';
+import Image from 'next/image';
+import BackgroundImage from '../../public/pics/book_bubbly_sean.jpeg';
 import { useIsTabletOrMobile } from '../../hooks';
 import Footer from '../../components/Footer';
 
+const X = 4;
+function Garden() {
+  return (
+    <Image src={BackgroundImage} alt="" width={6032 / X} height={3024 / X} />
+  );
+}
+
+const Y = 4;
+function GardenMobile() {
+  return (
+    <Image src={BackgroundImage} alt="" width={4032 / Y} height={3024 / Y} />
+  );
+}
+
 const Container = styled.div`
-  padding: 0;
-  margin: 0;
-  font-size: 2.5rem;
+  position: absolute;
+  top: 6rem;
+  width: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  background: linear-gradient(to right, #122932, 50%, #e3c0d3 50%);
-  height: 100vh;
-`;
-
-const Heading = `Sean Keegan`;
-
-const Title = styled.h1`
-  position: fixed;
-  padding: 1.4rem;
-  top: 0;
-  margin: 5rem;
-  font-size: 3rem;
-  background-color: white;
-  /* This is the index font */
-  font-family: 'Orbitron', sans-serif;
-`;
-
-const StyledMusic = styled.h2`
-  margin-top: 30rem;
-  padding-right: 20rem;
-  text-align: center;
-  font-family: 'Bangers', cursive;
-`;
-
-const StyledTutor = styled.h2`
-  margin-top: 30rem;
-  padding-left: 20rem;
-  text-align: center;
-  font-family: 'Happy Monkey', cursive;
-`;
-
-const TutorQuote = `"Best Tutor EVER!!"`;
-const MusicQuote = `"Incredible, a must see!"`;
-
-const MusicParagraph = styled.p`
-  margin-top: 9rem;
-  color: white;
-`;
-const TutorParagraph = styled.p`
-  margin-top: 9rem;
-  color: white;
 `;
 
 const StyledFooterContainer = styled.div`
@@ -79,22 +53,15 @@ function HomePage({ copy }) {
   const isTabletOrMobile = useIsTabletOrMobile();
   return (
     <Container>
-      <Title>{Heading}</Title>
-      {!isTabletOrMobile && (
+      {!isTabletOrMobile && <Garden />}
+      {isTabletOrMobile && (
         <>
-          <StyledMusic>
-            <Link href="/music/">Music</Link>
-            <MusicParagraph>{MusicQuote}</MusicParagraph>
-          </StyledMusic>
-          <StyledTutor>
-            <Link href="/tutoring/">Tutoring</Link>
-            <TutorParagraph>{TutorQuote}</TutorParagraph>
-          </StyledTutor>
-          {isTabletOrMobile && (
-            <StyledFooterContainer>
-              <Footer copy={copy} />
-            </StyledFooterContainer>
-          )}
+          <div>
+            <GardenMobile />
+          </div>
+          <StyledFooterContainer>
+            <Footer copy={copy} />
+          </StyledFooterContainer>
         </>
       )}
     </Container>
