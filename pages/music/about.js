3ea7e080d9a105aaef67e1e001b5c61af34aefde
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2022-02-23 19:26:23 -0500

    merge about/music

--- pages/music/about.js
+++ pages/music/about.js
@@ -1,17 +1,12 @@
 import styled from 'styled-components';
 import Image from 'next/image';
-import { useIsTabletOrMobile } from '../hooks';
+import { useIsTabletOrMobile } from '../../hooks';
 
-const AboutTitle = `Sean Chanel`;
+const AboutTitle = `Sean Keegan`;
 
 const AboutBlurb = `
-Hello! I’m Sean.
-
-I’m a graduate of the Evergreen State College, where my focus was on native plants, organic farming and printmaking. Since graduating in 2013, I’ve kept my hands in the soil and followed the lead of the many wonderful mentors I’ve been lucky enough to know along the way. I’ve had the privilege to work on beautiful estates and gardens from Seattle to Olympia and between. In 2019, through the encouragement of coworkers and clients, I started my own landscaping business.\n
-
-I offer garden designs that focus on enhancing the environment for my clients as well as pollinators and other wildlife. My goal as a designer is to bring the client's personality into a garden. Whether that’s an entire rainbow of phlox and echinacea or a monochromatic moon garden. I look forward to meeting each new challenge and seeing a project through. While my specialty is plant design, I’m happy to work with other local contractors to create the space you want to spend more time in.\n
-
-When I’m not in my client’s gardens, I’m usually in mine with my toddler. When I’m home, you can find me knitting, sewing or remodeling my home and watching my favorite reality TV shows 90 Day Fiance or The Bachelor.`;
+Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.;
+Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.Hello! Im Sean. I play awesome music! Let's talk. Hello! Im Sean. I play awesome music! Let's talk.`;
 
 const Container = styled.div`
   position: absolute;
@@ -19,6 +14,8 @@ const Container = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  /* music about page is having margin issues with the margin-top below- fix later */
+  margin-top: 7rem; 
   @media (max-width: 1438px) {
     /* position: absolute; */
     flex-direction: column;
@@ -31,10 +28,10 @@ const X = 3;
 function Family() {
   return (
     <Image
-      src="/pics/sean_new_about_foto.jpeg"
-      alt="the landscape artist standing next to a very large green plant"
-      width={15500 / X}
-      height={19000 / X}
+      src="/pics/sean_website.jpeg"
+      alt="the musician holding a classical guitar sitting on a stone garden bed in a well groomed park"
+      width={18324 / X}
+      height={17424 / X}
     />
   );
 }
