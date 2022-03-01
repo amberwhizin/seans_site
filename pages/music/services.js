7bba953ffb0fbb0ca158126836694b044fdf63fc
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2022-03-01 11:51:54 -0500

    services merge

--- pages/music/services.js
+++ pages/music/services.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 import Image from 'next/image';
-import BeforeCentral from '../public/pics/CentralStreetC.jpg';
-import AfterCentral from '../public/pics/CentralStreetB.jpg';
-import BeforeTullis from '../public/pics/TullisStreetIMG_0381.png';
-import AfterTullis from '../public/pics/TullisStreetIMG_0374.jpg';
-import _5thAveMaintenance from '../public/pics/5thAveIMG_8708.jpg';
-import _5thAveMaintenance_2 from '../public/pics/5thAveC.jpg';
+import BeforeCentral from '../../public/pics/CentralStreetC.jpg';
+import AfterCentral from '../../public/pics/CentralStreetB.jpg';
+import BeforeTullis from '../../public/pics/TullisStreetIMG_0381.png';
+import AfterTullis from '../../public/pics/TullisStreetIMG_0374.jpg';
+import _5thAveMaintenance from '../../public/pics/5thAveIMG_8708.jpg';
+import _5thAveMaintenance_2 from '../../public/pics/5thAveC.jpg';
 
 const Container = styled.div`
   position: absolute;
@@ -49,45 +49,7 @@ const ImageGrouping = styled.div`
 `;
 
 function Services() {
-  return (
-    <Container>
-      <StyledH2>Design</StyledH2>
-      <ImageGrouping>
-        <ServicesImage
-          src={BeforeTullis}
-          alt="An image of a persons garden before it's redone. It's on a small hill with two rows of stone"
-        />
-        <ServicesImage
-          src={AfterTullis}
-          alt="An image of a persons garden before it's redone. It's on a small hill with two rows of stone separating the hill where the plants will go."
-        />
-      </ImageGrouping>
-
-      <StyledH2>Install</StyledH2>
-      <ImageGrouping>
-        <ServicesImage
-          src={BeforeCentral}
-          alt="An image of a persons garden before it's redone. It's on a small hill with two rows of stone"
-        />
-        <ServicesImage
-          src={AfterCentral}
-          alt="An image of a persons garden before it's redone. It's on a small hill with two rows of stone separating the hill where the plants will go."
-        />
-      </ImageGrouping>
-
-      <StyledH2>Maintenance</StyledH2>
-      <ImageGrouping>
-        <ServicesImage
-          src={_5thAveMaintenance}
-          alt="An image of a persons garden before it's redone. It's on a small hill with two rows of stone"
-        />
-        <ServicesImage
-          src={_5thAveMaintenance_2}
-          alt="An image of a persons garden before it's redone. It's on a small hill with two rows of stone separating the hill where the plants will go."
-        />
-      </ImageGrouping>
-    </Container>
-  );
+  return <h1>Services</h1>;
 }
 
 export default Services;
