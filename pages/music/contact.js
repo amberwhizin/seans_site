8bda180a96c9d3f5876680d913e2acbca8c73ee3
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2022-03-01 11:53:04 -0500

    contact merge/ loacltion issue only

--- pages/music/contact.js
+++ pages/music/contact.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useIsTabletOrMobile } from '../hooks';
+import { useIsTabletOrMobile } from '../../hooks';
 
 const Container = styled.div`
   text-align: center;
