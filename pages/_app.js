bf15f99eb49225892a927ff3881f34bf828b6b13
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2022-04-28 10:04:48 -0400

    h1 tutotrin header change

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,11 @@ const GlobalStyle = createGlobalStyle`
   }
   h1 {
     font-size: 3rem;
-    color: #323232;
+    color:  ${(props) => {
+      if (props.isTutoring) {
+        return '#323232';
+      }
+    }};
     font-family: futura-pt, sans-serif;
     font-weight: 100;
   }
